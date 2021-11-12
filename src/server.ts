960fa381bbabd355c00ae0a49579d509eb3bba55
henryque <henryque_1@hotmail.com>
2021-11-12 18:23:48 -0300

    feat: add categories routes and post method to handle categories creation

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import express from "express";
+import { CategoriesRoutes } from "./categories.routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/courses', (request, response) => {
-  const { name } = request.body;
-
-  return response.json({ name });
-});
+app.use(CategoriesRoutes);
 
 app.listen(3333, () => console.log('Server is running!'));
