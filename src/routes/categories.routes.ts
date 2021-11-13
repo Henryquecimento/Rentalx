9a75795a69133e06de12e2990e23f78a8f728955
henryque <henryque_1@hotmail.com>
2021-11-13 19:28:10 -0300

    feat(CategoriesRepository): add findByName method to handle categories validation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -15,6 +15,13 @@ CategoriesRoutes.get("/", (request, response) => {
 CategoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists)
+    return response.status(400).json({
+      error: "Category already exists!",
+    });
+
   categoriesRepository.create({
     name,
     description,
