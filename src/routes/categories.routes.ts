e869422706cf23981895dc2bb12fd78bb6651efb
henryque <henryque_1@hotmail.com>
2021-11-18 17:43:36 -0300

    feat(categories.routes): add multer configs to handle imports

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoriesController } from "../modules/cars/useCases/CreateCategories/index";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const CategoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 CategoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
@@ -11,6 +16,12 @@ CategoriesRoutes.get("/", (request, response) => {
 
 CategoriesRoutes.post("/", (request, response) => {
   return createCategoriesController.handle(request, response);
+});
+
+CategoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log(file);
+  return response.send();
 });
 
 export { CategoriesRoutes };
