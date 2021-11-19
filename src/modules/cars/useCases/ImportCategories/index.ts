f8d13b87b93c8d07fcc2df4526545dd37be72f9f
henryque <henryque_1@hotmail.com>
2021-11-19 17:49:04 -0300

    refactor(ImportCategories/index): readjust methods to handle categories repository

--- src/modules/cars/useCases/ImportCategories/index.ts
+++ src/modules/cars/useCases/ImportCategories/index.ts
@@ -1,7 +1,12 @@
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 import { ImportCategoriesController } from "./ImportCategoriesController";
 import { ImportCategoriesUseCases } from "./ImportCategoriesUseCases";
 
-const importCategoriesUseCases = new ImportCategoriesUseCases();
+const categoriesRepository = CategoriesRepository.getInstance();
+
+const importCategoriesUseCases = new ImportCategoriesUseCases(
+  categoriesRepository
+);
 
 const importCategoriesController = new ImportCategoriesController(
   importCategoriesUseCases
