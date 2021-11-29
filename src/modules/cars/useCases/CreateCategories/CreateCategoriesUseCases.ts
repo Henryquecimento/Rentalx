be713750a56b3a6d7a31620e92828dc90756e1ba
henryque <henryque_1@hotmail.com>
2021-11-29 20:47:32 -0300

    refactor(CreateCategoriesUseCases): readjust to handle dependency injection

--- src/modules/cars/useCases/CreateCategories/CreateCategoriesUseCases.ts
+++ src/modules/cars/useCases/CreateCategories/CreateCategoriesUseCases.ts
@@ -1,11 +1,17 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRepository {
   name: string;
   description: string;
 }
+@injectable()
 class CreateCategoryUseCases {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
   async execute({ name, description }: IRepository): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(
