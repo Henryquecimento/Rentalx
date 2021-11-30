70aa3c49a36de9ee2cbab67ecdb0d30e8ab72a6f
henryque <henryque_1@hotmail.com>
2021-11-30 18:37:39 -0300

    refactor(ListCategoriesUseCases): readjust constructor and classes to handle category instance injection

--- src/modules/cars/useCases/ListCategories/ListCategoriesUseCases.ts
+++ src/modules/cars/useCases/ListCategories/ListCategoriesUseCases.ts
@@ -1,11 +1,19 @@
+import { inject, injectable } from "tsyringe";
+
 import { Category } from "../../entities/Category";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+@injectable()
 class ListCategoryUseCases {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository
+  ) {}
 
   async execute(): Promise<Category[]> {
-    return this.categoriesRepository.list();
+    const categories = await this.categoriesRepository.list();
+
+    return categories;
   }
 }
 
