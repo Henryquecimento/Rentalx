ac9e68d7658ccd733ca8fe610b7ae98d85c5aee3
henryque <henryque_1@hotmail.com>
2021-12-03 18:47:13 -0300

    refactor(CreateSpecificationsController): readjust to handle error messages

--- src/modules/cars/useCases/CreateSpecifications/CreateSpecificationsController.ts
+++ src/modules/cars/useCases/CreateSpecifications/CreateSpecificationsController.ts
@@ -7,13 +7,19 @@ class CreateSpecificationsController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    const createSpecificationsUseCases = container.resolve(
-      CreateSpecificationsUseCases
-    );
+    try {
+      const createSpecificationsUseCases = container.resolve(
+        CreateSpecificationsUseCases
+      );
 
-    await createSpecificationsUseCases.execute({ name, description });
+      await createSpecificationsUseCases.execute({ name, description });
 
-    return response.status(201).send();
+      return response.status(201).send();
+    } catch (error) {
+      return response
+        .status(500)
+        .json({ error: "Specification already exists" });
+    }
   }
 }
 
