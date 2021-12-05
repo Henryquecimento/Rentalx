dfd6ba79d57b87860c4fd210a2c00ec5688986b0
henryque <henryque_1@hotmail.com>
2021-12-05 20:02:27 -0300

    feat: add CreateUsers controller and useCases

---
+++ src/modules/accounts/useCases/CreateUsers/CreateUsersController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateUsersUseCases } from "./CreateUsersUseCases";
+
+class CreateUsersController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, username, email, password, driver_license } = request.body;
+
+    try {
+      const createUsersUseCases = container.resolve(CreateUsersUseCases);
+
+      await createUsersUseCases.execute({
+        name,
+        username,
+        email,
+        password,
+        driver_license,
+      });
+
+      return response.status(201).send();
+    } catch (error) {
+      return response.json(500).json({ error: "Users already exists" });
+    }
+  }
+}
+
+export { CreateUsersController };
