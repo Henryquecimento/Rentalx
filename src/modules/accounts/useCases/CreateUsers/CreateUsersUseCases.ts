9778cbc0c7827ffd1d6a9da79cd7bc283e5b7827
henryque <henryque_1@hotmail.com>
2021-12-06 17:57:01 -0300

    feat: add findByEmail method to handle existent user validation

--- src/modules/accounts/useCases/CreateUsers/CreateUsersUseCases.ts
+++ src/modules/accounts/useCases/CreateUsers/CreateUsersUseCases.ts
@@ -17,6 +17,12 @@ class CreateUsersUseCases {
     password,
     driver_license,
   }: ICreateUsersDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
     const encryptedPassword = await hash(password, 8);
 
     await this.usersRepository.create({
