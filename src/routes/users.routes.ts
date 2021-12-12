c77f9ce6bd875955f75184f1b2e8d9035d93ca1b
henryque <henryque_1@hotmail.com>
2021-12-12 20:41:04 -0300

    refactor(users.routes): readjust to handle new upload files config

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUsersController } from "../modules/accounts/useCases/CreateUsers/CreateUsersController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController";
 
 const UsersRoutes = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUsersController = new CreateUsersController();
 
+const updateUserAvatarController = new UpdateUserAvatarController();
+
 UsersRoutes.post("/", createUsersController.handle);
+
+UsersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { UsersRoutes };
