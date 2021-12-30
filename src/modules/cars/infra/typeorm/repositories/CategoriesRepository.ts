e2155d9930eb9b0636b8c704ab4194f5127e6079
henryque <henryque_1@hotmail.com>
2021-12-30 17:24:04 -0300

    refactor: separate typeorm implementations in cars

---
+++ src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-use-before-define */
+import { getRepository, Repository } from "typeorm";
+
+import {
+  ICategoriesRepository,
+  ICreateCategoriesDTO,
+} from "@modules/cars/repositories/ICategoriesRepository";
+
+import { Category } from "../entities/Category";
+
+class CategoriesRepository implements ICategoriesRepository {
+  private repository: Repository<Category>;
+
+  constructor() {
+    this.repository = getRepository(Category);
+  }
+
+  async create({ name, description }: ICreateCategoriesDTO): Promise<void> {
+    const category = this.repository.create({
+      name,
+      description,
+    });
+
+    await this.repository.save(category);
+  }
+
+  async list(): Promise<Category[]> {
+    const categories = await this.repository.find();
+
+    return categories;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    const category = await this.repository.findOne({ name });
+
+    return category;
+  }
+}
+
+export { CategoriesRepository };
---
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -0,0 +1,33 @@
+import { getRepository, Repository } from "typeorm";
+
+import {
+  ISpacificationRepository,
+  ICreateSpecificationDTO,
+} from "@modules/cars/repositories/ISpecificationsRepository";
+
+import { Specification } from "../entities/Specification";
+
+class SpecificationRepository implements ISpacificationRepository {
+  private repository: Repository<Specification>;
+
+  constructor() {
+    this.repository = getRepository(Specification);
+  }
+
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
+      name,
+      description,
+    });
+
+    await this.repository.save(specification);
+  }
+
+  async findByName(name: string): Promise<Specification> {
+    const specification = await this.repository.findOne({ name });
+
+    return specification;
+  }
+}
+
+export { SpecificationRepository };
