6c7012df6d6755b3547af20579db306abb94fa03
henryque <henryque_1@hotmail.com>
2022-01-08 14:25:32 -0400

    feat: add findAvailable method to list cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -4,7 +4,6 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "../ICarsRepository";
 
 class CarsRepositoryInMemory implements ICarsRepository {
-
   cars: Car[] = [];
 
   async create({
@@ -37,6 +36,26 @@ class CarsRepositoryInMemory implements ICarsRepository {
     return this.cars.find((car) => {
       return car.license_plate === license_plate;
     });
+  }
+
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const carsAvailable = this.cars.filter((car) => {
+      if (
+        car.available === true &&
+        ((brand && car.brand === brand) ||
+          (category_id && car.category_id === category_id) ||
+          (name && car.name === name))
+      ) {
+        return car;
+      }
+      return null;
+    });
+
+    return carsAvailable;
   }
 }
 
