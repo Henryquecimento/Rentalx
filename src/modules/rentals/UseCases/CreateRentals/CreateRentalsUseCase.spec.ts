8591f1603d83fe268ce702ff22b50bb3682e7d7c
henryque <henryque_1@hotmail.com>
2022-01-15 16:34:24 -0400

    chore: add CreateRentalsUseCase test file to handle rentals test

---
+++ src/modules/rentals/UseCases/CreateRentals/CreateRentalsUseCase.spec.ts
@@ -0,0 +1,21 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+
+import { CreateRentalUseCase } from "./CreateRentalsUseCase";
+
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let createRentalUseCase: CreateRentalUseCase;
+
+describe("Create a Car Rental", () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it("should be able to create a car rental", async () => {
+    await createRentalUseCase.execute({
+      car_id: "1234",
+      user_id: "4321",
+      expected_return_date: new Date(),
+    });
+  });
+});
