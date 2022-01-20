66449f85672dedd6c5119300857f43d771402adc
henryque <henryque_1@hotmail.com>
2022-01-20 18:38:28 -0400

    feat(CreateRentalUseCase): add dayjs lib to handle minimum hours to return rental  validation

--- src/modules/rentals/UseCases/CreateRentals/CreateRentalsUseCase.ts
+++ src/modules/rentals/UseCases/CreateRentals/CreateRentalsUseCase.ts
@@ -1,7 +1,11 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRental";
 import { AppError } from "@shared/errors/AppError";
 
+dayjs.extend(utc);
 interface IRequest {
   car_id: string;
   user_id: string;
@@ -16,6 +20,8 @@ class CreateRentalUseCase {
     user_id,
     expected_return_date,
   }: IRequest): Promise<Rental> {
+    const minimumHour = 24;
+
     const carUnvailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id
     );
@@ -28,6 +34,17 @@ class CreateRentalUseCase {
 
     if (rentalOpenToUser)
       throw new AppError("There's a rental in progress for this user");
+
+    const expectedReturnDateFormat = dayjs(expected_return_date)
+      .utc()
+      .local()
+      .format();
+
+    const dateNow = dayjs().utc().local().format();
+
+    const compare = dayjs(expectedReturnDateFormat).diff(dateNow, "hours");
+
+    if (compare < minimumHour) throw new AppError("Invalid return time");
 
     const rental = await this.rentalsRepository.create({
       car_id,
