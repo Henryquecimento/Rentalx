c8f73a9a1a88f4064e274c6d9dc4248908f055b5
henryque <henryque_1@hotmail.com>
2022-02-12 16:58:59 -0400

    chore: add Auth config

---
+++ src/config/Auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "1baa845a2c8612cc3740141ee4c07683",
+  expires_in_token: "15m",
+  secret_refresh_token: "50f5f5597a9b294e0f7ec52b4ce3bf22",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
