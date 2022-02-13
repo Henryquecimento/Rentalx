f488f370b86c7219cb888bf3d1ae25a74923ec10
henryque <henryque_1@hotmail.com>
2022-02-13 15:04:08 -0400

    feat: add refresh token routes

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/AuthenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/RefreshToken/RefreshTokenController";
 
 const AuthenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 AuthenticateRoutes.post("/", authenticateUserController.handle);
+AuthenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { AuthenticateRoutes };
