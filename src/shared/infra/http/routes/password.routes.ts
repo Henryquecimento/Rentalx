17ef71496345e47a9f7d6e25d83cce439decb838
henryque <henryque_1@hotmail.com>
2022-02-15 15:38:08 -0400

    feat: add reset password routes

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/useCases/ResetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/SendForgotPasswordMail/SendForgotPasswordMailController";
 
 const PasswordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 PasswordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+PasswordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { PasswordRoutes };
